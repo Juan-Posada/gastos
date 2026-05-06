4a5f21c640476d7f65d1807f3f8ce4951a97e6ac
Juan-Posada <juanposada.0724@gmail.com>
2026-05-05 22:41:55 -0500

    Add: Se cambiaron estilos principales del proyecto y se creo método de acceso por medio de sesiones de google

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,17 @@
+/* ══════════════════════════════════════════════════════
+   FIREBASE CONFIG
+   ► Reemplaza estos valores con los de tu proyecto Firebase
+   ► Guía de configuración al final del README
+══════════════════════════════════════════════════════ */
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBWxOtOxy3Tn3vUL9OkmlINYhoPG8moLNI",
+    authDomain: "finza-b2a7d.firebaseapp.com",
+    projectId: "finza-b2a7d",
+    storageBucket: "finza-b2a7d.firebasestorage.app",
+    messagingSenderId: "717438921070",
+    appId: "1:717438921070:web:ddece5d01f1c026f61797f",
+    measurementId: "G-N2ZKB78ML7"
+};
+
+export default FIREBASE_CONFIG;
